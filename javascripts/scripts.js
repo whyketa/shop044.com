3105cc1c3343bc1f38ba2ad8c406aec595aa7681
whyketa <dalomukhina@edu.hse.ru>
2026-03-28 22:48:33 +0300

    ^-^

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,1 +1,21 @@
 //console.log("JS есть");//
+const openBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeModal");
+
+// открыть
+openBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+// закрыть по крестику
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+// закрыть при клике вне окна
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("active");
+  }
+});
